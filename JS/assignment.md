58488bbc2407862aa5fbb4cd6eced6cd5bb1713f
vmalepati <vmalepati@hostanalytics.com>
2018-11-20 09:03:19 +0530

    JS

---
+++ JS/assignment.md
@@ -0,0 +1,33 @@
+1) a>b or b>a
+2) user input and print all even number between 1 and user input
+3) user input and print all odd number between 1 and user input
+4) user input and print fibonacci series
+5) user input and print *s
+
+input is 5 ==>
+
+*
+**
+***
+****
+*****                
+
+input is 5 ==>
+
+*****                
+****
+***
+**
+*
+
+input is 5 ==>
+
+*****                
+****
+***
+**
+*
+**
+***
+****
+*****
--- JS/script.js
+++ JS/script.js
@@ -3,8 +3,49 @@ var b = 2;
 
 if(a > b){
     alert(a);
- }
-
- if(b > a){
+}
+else if(a < b){
     alert(b);
- }
+}
+else {
+    alert("a & b are equal");
+}
+
+
+var a = prompt("Enter a");
+var b = prompt("Enter b");
+
+a = parseInt(a);
+b = parseInt(b);
+
+if(a > b){
+    alert(a + " is greater");
+}
+else if(a < b){
+    alert(b + " is greater");
+}
+else {
+    alert("a & b are equal");
+}
+
+
+var tableNumber = prompt("Enter a number to get table");
+
+tableNumber = parseInt(tableNumber);
+
+//    console.log(tableNumber + " X 1 = " + (tableNumber * 1));
+//    //    2 + "X 1 =" + 2 ==> "2" + "X 1 =" + "2" ==> "2 X 1 = 2"
+//    console.log(tableNumber + " X 2 = " + (tableNumber * 2));
+//    console.log(tableNumber + " X 3 = " + (tableNumber * 3));
+//    console.log(tableNumber + " X 4 = " + (tableNumber * 4));
+//    console.log(tableNumber + " X 5 = " + (tableNumber * 5));
+//    console.log(tableNumber + " X 6 = " + (tableNumber * 6));
+//    console.log(tableNumber + " X 7 = " + (tableNumber * 7));
+//    console.log(tableNumber + " X 8 = " + (tableNumber * 8));
+//    console.log(tableNumber + " X 9 = " + (tableNumber * 9));
+//    console.log(tableNumber + " X 10 = " + (tableNumber * 10));
+
+
+for(var i=1; i <= 10; i++ ){
+ console.log(tableNumber + " X " + i + " = " + (tableNumber * i));
+}
